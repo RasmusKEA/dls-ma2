9c1d8e46243528ad8d1a835a380a8e75702f3755
Rasmus Møller <rasm937k@stud.kea.dk>
2023-04-23 17:25:57 +0200

    login page

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static/html/login.html'))
+  res.sendFile(path.join(__dirname, 'public/html/login.html'))
 })
 
 app.listen(3000, () => {
